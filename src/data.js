cee137316ea2a629aa288c2678926f86440700f0
Bikash Santra <89884891+BikashSantra03@users.noreply.github.com>
2024-01-15 14:12:47 +0530

    All Files uploaded successfully

---
+++ src/data.js
@@ -0,0 +1,50 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Bikash Santra",
+    job: "Software Engineer",
+    image: "https://avatars.githubusercontent.com/u/89884891?v=4",
+    text: "Working with this team was a pleasure. Their technical expertise and ability to deliver quality solutions on time and within budget were impressive. I highly recommend them as a true partner and asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Sampurna Pandit",
+    job: "Software Engineer",
+    image:
+      "https://scontent-ccu1-1.xx.fbcdn.net/v/t1.6435-9/91288514_913878675748720_3716450336693551104_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=YJJ_AspShMsAX8PGkuK&_nc_ht=scontent-ccu1-1.xx&oh=00_AfARR56oYIRJ6YXKykXU0Jn_3JfG4w3gd4FGwzSQ3c0Uuw&oe=65CC46E5",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 3,
+    name: "Rahul Sahoo",
+    job: "Cloud Engineer",
+    image:
+      "https://scontent-ccu1-1.xx.fbcdn.net/v/t39.30808-6/394728599_1282155512464433_1121190232639549808_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=UgkI7KUPcTgAX_xO7O9&_nc_ht=scontent-ccu1-1.xx&oh=00_AfD0miz2GsZ68uCYxnVFQbZOATLGfDnSsMV8Z7UGXgzuRA&oe=65A94059",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 4,
+    name: "Soumya Banerjee",
+    job: "Marketing Manager",
+    image:
+      "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 5,
+    name: "Saikat Mukherjee",
+    job: "Content Strategist",
+    image:
+      "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 6,
+    name: "Aritra Biswas",
+    job: "Data Analyst",
+    image: "https://avatars.githubusercontent.com/u/93366359?v=4",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
